a25275b90c1efa66ca5f205bfe5eb888440d7ffb
Sergio Faya Fernández <UO251005@uniovi.es>
2017-02-22 16:54:21 +0100

    lab 4

---
+++ Lab4/src/alg77777777/s3/Subtraction4.java
@@ -0,0 +1,32 @@
+package alg77777777.s3;
+
+public class Subtraction4 {
+	public static long rec3(int n)
+	{
+	 long cont = 0;
+	 if (n<=0) cont++;
+	 else
+	  {
+	   cont++;  //O(1)    
+	   rec3(n-2);
+	   rec3(n-2);
+	   rec3(n-2);
+	  }
+	 return cont;   
+	}
+	
+	public static void main(String arg []) 
+	{
+	 long t1,t2,cont=0;
+	 for (int n=1;n<=100;n++)
+	 {
+	  t1 = System.currentTimeMillis();
+	   
+	  cont=rec3(n);
+	 
+	  t2 = System.currentTimeMillis();
+	
+	  System.out.println ("n="+n+ "**TIME="+(t2-t1)+"**cont="+cont);
+	 }  // for
+	} // main
+}
